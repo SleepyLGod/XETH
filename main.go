ef702ff092890fdb1a63329e6145bbbfba0600e1
odin <2441164168lhd@gmail.com>
2022-10-13 18:39:54 +0800

    database config modified

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"XETH/config"
-	"XETH/dbDriver"
 	xethServer "XETH/server"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -25,32 +22,5 @@ func main() {
 		return
 	})
 
-	databaseLogin := config.DatabaseGenerator
-	driver, err := dbDriver.NewMysqlDriver(databaseLogin().UsrName, databaseLogin().Psw, databaseLogin().Addr, databaseLogin().DbName, &User{})
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// testings
-	u := &User{Name: "333"}
-	if err = driver.Create(u); err != nil {
-		fmt.Println(err)
-	}
-	var u2 []User
-	con := make([]dbDriver.QueryConstraint, 0)
-
-	con = append(con, dbDriver.QueryConstraint {
-		FieldName: "name",
-		Operator:  ">",
-		Value:     "1",
-	})
-	con = append(con, dbDriver.QueryConstraint {
-		FieldName: "name",
-		Operator:  "!=",
-		Value:     "test",
-	})
-	driver.Query(&u2, con)
-	fmt.Println(len(u2))
-	driver.CloseDB()
 	server.StartAndListen(":88")
 }
